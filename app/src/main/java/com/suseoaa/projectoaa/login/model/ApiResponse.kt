66382159dfa1552f48c37208e4457e833c7ffe44
Saya <2651246483@qq.com>
2025-12-03 12:07:15 +0800

    Remove all files from remote

--- app/src/main/java/com/suseoaa/projectoaa/login/model/ApiResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.suseoaa.projectoaa.login.model
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ApiResponse<T>(
-    val code: Int,
-    val message: String,
-    val data: T?
-)
